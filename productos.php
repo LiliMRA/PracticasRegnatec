1dc3771f964c26433eca661483804d7a3068e76e
LiliMRA <lilianmishellrivera@gmail.com>
2024-05-25 10:18:34 +0200

    Archivos modificados viernes 24-05

---
+++ productos.php
@@ -0,0 +1,12 @@
+<php?
+
+$servidor= "127.0.0.1";
+$baseDeDatos = "bytestore";
+$usuario = "root";
+$contrasenia = "";
+
+try {
+    $conexion = new PDO("mysql:host=$seridor; dbname=baseDeDatos", $usuario, $contrasenia);
+} catch (Exception $ex) {
+    echo $ex -> getMessage();
+    ?>
